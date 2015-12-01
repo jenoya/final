6731fc296fe2a37ce1523a07bdf691fcd4a1f4fc
jenoya <jennifer.oya.101@my.csun.edu>
2015-11-30 19:55:47 -0800

    acf and theme edits
    added advanced custom fields to plugins
    tried to make an image appear in html... unsuccessful...

---
+++ wp-content/themes/art405/header.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title><?php if(is_home()) bloginfo('name'); else bloginfo('name'); wp_title(''); ?></title>
+
+	<link rel="stylesheet" type="text/css" href="<?php bloginfo('template_directory'); ?>/style.css">
+	<link rel="stylesheet" type="text/css" href="<?php bloginfo('template_directory'); ?>/css/bootstrap.min.css">
+	<!-- JAVA JQUERY -->
+	<script src="http://code.jquery.com/jquery-1.11.0.min.js"></script>
+	<script src="http://code.jquery.com/jquery-migrate-1.2.1.min.js"></script>
+<!-- BOOTSTRAP JQUERY -->
+	<script type="text/javascript" src="<?php bloginfo('template_directory'); ?>/js/bootstrap.min.js"></script>
+
+
+	<?php wp_head();?>
+
+</head>
+<body>
+
+	
---
+++ wp-content/themes/art405/index.php
@@ -0,0 +1,11 @@
+<?php get_header(); ?>
+
+	<img src="land.jpg" alt="background img"/>
+
+<div class="col-xs-12 searchbar">
+	<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Maxime modi atque saepe soluta quidem natus, quam ducimus id ipsa incidunt dolores corrupti a non possimus, sequi quod cupiditate quos sit!</p>
+</div>
+
+
+
+<?php get_footer(); ?>
