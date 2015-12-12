6235bc424aefa66ec3b3714b1d31c5c2c3d7904f
maxxr24 <max.murillo.408@my.csun.edu>
2015-12-12 13:58:55 -0800

    Updated Footer and Header for Nav
    I added bootstrap scripts to run bootstrap functions. Added basic Nav,
    needs to be styled for theme.

--- wp-content/themes/art405/header.php
+++ wp-content/themes/art405/header.php
@@ -10,10 +10,39 @@
 <!-- BOOTSTRAP JQUERY -->
 	<script type="text/javascript" src="<?php bloginfo('template_directory'); ?>/js/bootstrap.min.js"></script>
 	<link rel="stylesheet" type="text/css" href="<?php bloginfo('template_directory'); ?>/style.css">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
 
 	<?php wp_head();?>
 
 </head>
 <body>
 
-	
+<nav class="navbar navbar-default" role="navigation">
+	<!-- Brand and toggle get grouped for better mobile display -->
+	<div class="navbar-header">
+		<button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-ex1-collapse">
+			<span class="sr-only">Toggle navigation</span>
+			<span class="icon-bar"></span>
+			<span class="icon-bar"></span>
+			<span class="icon-bar"></span>
+		</button>
+		<a href = "<?php bloginfo('url');?>" class = "navbar-brand">Stock Stalker</a>
+	</div>
+
+	<!-- Collect the nav links, forms, and other content for toggling -->
+	<div class="collapse navbar-collapse navbar-ex1-collapse">
+		<ul class="nav navbar-nav navbar-right">
+			<li class="dropdown">
+				<a href="#" class="dropdown-toggle" data-toggle="dropdown">Browse<b class="caret"></b></a>
+				<ul class="dropdown-menu">
+					<li><a href="#">Action</a></li>
+					<li><a href="#">Another action</a></li>
+					<li><a href="#">Something else here</a></li>
+					<li><a href="#">Separated link</a></li>
+				</ul>
+			</li>
+			<li><a href="#">Link</a></li>
+		</ul>
+	</div><!-- /.navbar-collapse -->
+</nav>
+
