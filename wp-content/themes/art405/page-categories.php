a74dc7a30478c4b6e51a5f8932918a05a2588044
jenoya <jennifer.oya.101@my.csun.edu>
2015-12-13 21:12:42 -0800

    styling for categories
    tweaking some stuff in the categories page

--- wp-content/themes/art405/page-categories.php
+++ wp-content/themes/art405/page-categories.php
@@ -6,16 +6,16 @@ Template Name: Categories Page
 <div class="container" style="background-color:white;">
 	<?php $category = new WP_Query(['post_type'=>'category-box']);
 	 	if ($category->have_posts()) : while ($category->have_posts()) : $category->the_post(); ?>
-		<div class="container" style="padding: 0px;">
-			<div class="col-md-12" style="padding: 0px;">
-				<img style="margin-top:30px; padding right:30px;"src="<?php the_field('background_image'); ?>" class="img-responsive">
-				<div class="carousel-caption">
-					<h3><?php the_title();?></h3>
-					<p><?php the_content(''); ?></p>
-				</div>
+	 		<div class="col-xs-12">
+	 			<a href="#">
+		 			<img style="margin-top:30px;"src="<?php the_field('background_image'); ?>" class="img-responsive">
+					<div class="carousel-caption" style="padding-bottom:0px;">
+						<h3 style="color: rgba(0,128,185,1);"><?php the_title();?></h3>
+						<p><?php the_content(''); ?></p>
+					</div>
+				</a>
 			</div>
-		
-		</div>
+
 	<?php endwhile; else: ?>
 	    <p>Sorry, no Category Boxes match your criteria</p>
 	<?php endif; ?>
