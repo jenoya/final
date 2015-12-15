cb341124104630d07d55897e1222d1651b6e1fa7
maxxr24 <max.murillo.408@my.csun.edu>
2015-12-14 16:59:21 -0800

    about

--- wp-content/themes/art405/page-about.php
+++ wp-content/themes/art405/page-about.php
@@ -4,25 +4,23 @@ Template Name: About Page
 */
 ?>
 <div class="container-full">
-	<div class="title-top-section">
+	<div class="row title-top-section text-center">
 		<h1 class="title-section-text"><?php the_title();?></h1>
 	</div>
 </div>
 <div class="container">
-	<div class="col-md-12">
+	<div class="col-xs-12">
 		<div class="panel panel-default panel-body">
 			<?php while (have_posts()) : the_post(); ?>
-				<div class="row center-box">
-					<div class="center-box thumbnail"><?php the_post_thumbnail('thumbnail'); ?></div>
-				</div>
-				<div class="row center-box">
-					<p><?php the_content(''); ?></p>
-				</div>
-			<?php endwhile; wp_reset_query(); ?>
+				<?php the_content(''); ?>
+			<?php endwhile; wp_reset_query(); ?> 
 		</div>
 	</div>
 </div>
 
 
 
+
+
 <?php get_footer(); ?>
+
